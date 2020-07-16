58f0a92c8ee440a1910f9895522c9eebfe5996df
Tiago Braga <deploy@vixtecnologia.com.br>
2020-07-15 22:48:59 -0300

    Add code to wsyg

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -40,8 +40,8 @@
                 tinymce.init({
                     selector: 'textarea',
                     plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed ' +
-                        'permanentpen powerpaste table advtable tinycomments tinymcespellchecker image',
-                    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table image',
+                        'permanentpen powerpaste table advtable tinycomments tinymcespellchecker image codesample',
+                    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table image codesample',
                     toolbar_mode: 'floating',
                     tinycomments_mode: 'embedded',
                     tinycomments_author: 'Author name',
